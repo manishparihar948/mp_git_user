9b1e02e817eda28a2940c8415d66fbc7dc247160
manishparihar948 <manish.parihar948@gmail.com>
2026-05-20 10:33:00 +0530

    Add Gemfile for Fastlane

---
+++ MPGitUserShowCase/MPGitUserShowCase/MPGitUserShowCaseApp.swift
@@ -0,0 +1,32 @@
+//
+//  MPGitUserShowCaseApp.swift
+//  MPGitUserShowCase
+//
+//  Created by Manish Parihar on 20.05.26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct MPGitUserShowCaseApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
